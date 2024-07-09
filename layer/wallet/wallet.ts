334ca7e4e6d52948d8cc0799aafe45f48a3ee5a0
shane-moore <skm1790@gmail.com>
2024-07-09 14:02:46 -0700

    chore: test7

--- layer/wallet/wallet.ts
+++ layer/wallet/wallet.ts
@@ -6,7 +6,9 @@ import {
 import { walletStrategy } from './wallet-strategy'
 
 export const getAddresses = async (): Promise<string[]> => {
+  console.log('before enableAndGetAddresses', walletStrategy.getWallet())
   const addresses = await walletStrategy.enableAndGetAddresses()
+  console.log('after enableAndGetAddresses', walletStrategy.getWallet())
 
   if (addresses.length === 0) {
     throw new WalletException(
